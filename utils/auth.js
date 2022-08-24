73eb69f45f51e6e082c235c06e09f6b5b45960d7
Promise Johnson <chiemelapromise30@gmail.com>
2022-08-24 07:57:35 -0700

    Add the authentication for user

--- utils/auth.js
+++ utils/auth.js
@@ -15,13 +15,18 @@ const { User } = require("../models/User.model");
 
 const auth = async (req, res, next) => {
   const token = req.header("Authorization");
-  if (!token) return res.status(400).json("Authorization Failed");
+  if (token=='undefined'){
+    console.log('no token found')
+    return res.status(400).json("Authorization Failed");
+  }
+  console.log(token=='undefined', 'the token dey o')
   try {
     const user = jwt.verify(token, process.env.ACCESS_TOKEN);
     req.user = user
     next();
   } catch (error) {
-    res.status(500).json("Not authorized");
+    console.log('not authorized')
+    res.status(500).json(error);
   }
 };
 // const auth = async (req, res, next) => {
